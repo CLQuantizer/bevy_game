9e49aecada3953cae6d5b8005f58626c2abfcdc0
CLQuantizer <ezioxxk@gmail.com>
2023-02-15 16:02:24 +0000

    ff

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugin(HelloPlugin)
         .run();
 }
-
 fn hello_world() {
     println!("hello world!");
 }
@@ -28,5 +25,15 @@ fn add_people(mut commands: Commands) {
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in query.iter() {
         println!("hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people);
     }
 }
